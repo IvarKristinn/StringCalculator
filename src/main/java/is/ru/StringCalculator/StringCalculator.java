7a5f453abc6b08261652471bf3b8f1cf36a83cbe
Ívar Kristinn Hallsson <hallssonivarkristinn@gmail.com>
2017-10-15 19:38:21 +0000

    refactored newline

--- src/main/java/is/ru/StringCalculator/StringCalculator.java
+++ src/main/java/is/ru/StringCalculator/StringCalculator.java
@@ -10,31 +10,19 @@ public class StringCalculator
   		return 0;
   	}
 
-  	else if(text.contains("\n") && text.contains(","))
+  	else if(text.contains("\n") || text.contains(","))
   	{
   		return sum(splitNumbersNewLineAndComma(text));
   	}
 
-  	else if(text.contains("\n"))
-  	{
-  		return sum(splitNumbersNewLine(text));
-  	}
-
-  	else if(text.contains(","))
-  	{
-  		return sum(splitNumbers(text));
-  	}
-  	
   	else
   	{
   		return 1;
   	}
-  	
   		
   }	
   	
   
-
   private static int toInt(String number)
   {
   	return Integer.parseInt(number);
@@ -48,16 +36,6 @@ public class StringCalculator
   		total = total + toInt(number);
 	}
   		return total;
-  }
-
-  private static String[] splitNumbers(String numbers)
-  {
-  	return numbers.split(",");
-  }
-
-  private static String[] splitNumbersNewLine(String numbers)
-  {
-  	return numbers.split("\n");
   }
 
   private static String[] splitNumbersNewLineAndComma(String numbers)
